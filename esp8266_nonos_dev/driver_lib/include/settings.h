6b41ceb2c7633ab5985ad231cae2031bc96d2387
matyaskiss <nolex90@gmail.com>
2018-02-05 18:34:29 +0100

    Orai_munka workspace set up.

---
+++ esp8266_nonos_dev/driver_lib/include/settings.h
@@ -0,0 +1,23 @@
+/*
+ * settings.h
+ *
+ *  Created on: Jan 23, 2018
+ *      Author: nolex
+ */
+
+#ifndef DEVELOPMENT_INCLUDE_SETTINGS_H_
+#define DEVELOPMENT_INCLUDE_SETTINGS_H_
+
+#define SSID "navtah"
+#define PASSWORD "qwas1234"
+
+#define FUNC_U0CTS    4
+#define FUNC_U0RTS    4
+
+#define LED_GPIO 4
+#define LED_GPIO_MUX PERIPHS_IO_MUX_GPIO4_U
+#define LED_GPIO_FUNC FUNC_GPIO4
+
+#define DELAY 1000 /* milliseconds */
+
+#endif /* DEVELOPMENT_INCLUDE_SETTINGS_H_ */
---
+++ esp8266_nonos_dev/orai_munka/user/.output/eagle/debug/obj/user_main.d
@@ -0,0 +1,21 @@
+.output/eagle/debug/obj/user_main.o .output/eagle/debug/obj/user_main.d : user_main.c ../../include/osapi.h \
+ /home/nolex/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/string.h \
+ /home/nolex/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/_ansi.h \
+ /home/nolex/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/newlib.h \
+ /home/nolex/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/config.h \
+ /home/nolex/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/ieeefp.h \
+ /home/nolex/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/features.h \
+ /home/nolex/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/xtensa/config/core-isa.h \
+ /home/nolex/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/reent.h \
+ /home/nolex/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/_ansi.h \
+ /home/nolex/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/_types.h \
+ /home/nolex/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/_types.h \
+ /home/nolex/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/_default_types.h \
+ /home/nolex/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/lock.h \
+ /home/nolex/esp-open-sdk/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.5/include/stddef.h \
+ /home/nolex/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/cdefs.h \
+ /home/nolex/esp-open-sdk/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/string.h \
+ ../../include/os_type.h ../../include/ets_sys.h ../../include/c_types.h \
+ ../../include/eagle_soc.h ../../driver_lib/include/user_config.h \
+ ../../include/user_interface.h ../../include/ip_addr.h \
+ ../../include/queue.h ../../include/spi_flash.h ../../include/gpio.h
