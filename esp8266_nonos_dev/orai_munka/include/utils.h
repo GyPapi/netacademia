b0a54f93e3d444cff45aafaa1428704271b93cf9
matyaskiss <nolex90@gmail.com>
2018-02-05 20:24:21 +0100

    Completing utils for esp8266

---
+++ esp8266_nonos_dev/orai_munka/include/utils.h
@@ -0,0 +1,25 @@
+/*
+ * utils.h
+ *
+ *  Created on: Feb 5, 2018
+ *      Author: nolex
+ */
+
+#ifndef ORAI_MUNKA_INCLUDE_UTILS_H_
+#define ORAI_MUNKA_INCLUDE_UTILS_H_
+
+#include "osapi.h"
+#include "user_interface.h"
+#include "../include/driver/uart.h"
+
+#define LED_GPIO_MUX
+#define LED_GPIO_FUNC
+
+//IO12 LED
+
+void initUtils();
+
+
+
+
+#endif /* ORAI_MUNKA_INCLUDE_UTILS_H_ */
---
+++ esp8266_nonos_dev/orai_munka/user/utils.c
@@ -0,0 +1,50 @@
+/*
+ * utils.c
+ *
+ *  Created on: Feb 5, 2018
+ *      Author: nolex
+ *      Implementing basic HeartBeat functionality.
+ */
+
+#include "utils.h"
+
+static uint8_t led_state;
+static os_timer_t blinkTimer;
+
+
+void heartBeat()
+{
+	GPIO_OUTPUT_SET(12, led_state);
+	led_state ^= 1;
+	os_printf("heartBeat!\n\r");
+}
+
+void initBlinky()
+{
+	 uart_div_modify(UART0, UART_CLK_FREQ/115200);
+	 led_state = 0;
+	 PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTDI_U, FUNC_GPIO12);
+	 os_timer_setfn(&blinkTimer, (os_timer_func_t*)heartBeat, 0);
+	 os_timer_arm(&blinkTimer, 500, 1);
+}
+
+void initConnection()
+{
+	wifi_set_opmode(0x01);
+	wifi_softap_dhcps_stop();
+	char ssid[]  = "navtah";
+	char password[] = "";
+	struct station_config stationConf;
+	stationConf.bssid_set = 0;
+	os_memcpy(&stationConf.ssid, ssid, 32);
+	os_memcpy(&stationConf.password, password, 64);
+	wifi_station_set_config(&stationConf);
+
+
+}
+
+void initUtils()
+{
+	initBlinky();
+	//initConnection();
+}
