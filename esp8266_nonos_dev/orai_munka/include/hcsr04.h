225c60d9329720dbc93227b55f50bddd4786b8c7
matyaskiss <nolex90@gmail.com>
2018-02-05 20:31:47 +0100

    End, committing bone of HCSR04 Sensor.

---
+++ esp8266_nonos_dev/orai_munka/include/hcsr04.h
@@ -0,0 +1,13 @@
+/*
+ * hcsr04.h
+ *
+ *  Created on: Feb 5, 2018
+ *      Author: nolex
+ */
+
+#ifndef ORAI_MUNKA_INCLUDE_HCSR04_H_
+#define ORAI_MUNKA_INCLUDE_HCSR04_H_
+
+
+
+#endif /* ORAI_MUNKA_INCLUDE_HCSR04_H_ */
---
+++ esp8266_nonos_dev/orai_munka/user/hcsr04.c
@@ -0,0 +1,28 @@
+/*
+ * hcsr04.c
+ *
+ *  Created on: Feb 5, 2018
+ *      Author: nolex
+ */
+
+
+void ultraSInit()
+{
+
+}
+
+void ultraSStart()
+{
+
+}
+
+void ultraSTask()
+{
+
+}
+
+void testUltraS()
+{
+	ultraSInit();
+	ultraSStart();
+}
