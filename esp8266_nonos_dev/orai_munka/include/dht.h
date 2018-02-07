d9207ef635c5ab72b6fe5126a62afcde25545a0c
matyaskiss <nolex90@gmail.com>
2018-02-07 20:33:32 +0100

    First incomplete version of DHT11 driver

--- esp8266_nonos_dev/orai_munka/include/dht.h
+++ esp8266_nonos_dev/orai_munka/include/dht.h
@@ -8,6 +8,15 @@
 #ifndef ORAI_MUNKA_INCLUDE_DHT_H_
 #define ORAI_MUNKA_INCLUDE_DHT_H_
 
+#include "osapi.h"
+#include "os_type.h"
+#include "user_interface.h"
+#include "ets_sys.h"
+#include "gpio.h"
+#include "user_interface.h"
+#include "c_types.h"
+
+
 #define DHT_PIN		14
 
 #define DHT11		1
@@ -15,7 +24,17 @@
 
 #define DHT_TYPE	DHT11
 
-#define DHT_PIN_ON()	GPIO_OUTPUT_SET()
+#define DHT_PIN_ON()	GPIO_OUTPUT_SET(DHT_PIN, 1)
+#define DHT_PIN_OFF()	GPIO_OUTPUT_SET(DHT_PIN, 0)
+
+#define DHT_PIN_DIS()	GPIO_DIS_OUTPUT(DHT_PIN)
+
+#define DHT_READ_PIN()	GPIO_INPUT_GET(DHT_PIN)
+
+#define READ_REG(_r)	(*(volatile uint32_t *)(_r))
+#define WDEW_NOW()		READ_REG(0x3ff20c00)
+
+
 
 
 #endif /* ORAI_MUNKA_INCLUDE_DHT_H_ */
--- esp8266_nonos_dev/orai_munka/user/dht.c
+++ esp8266_nonos_dev/orai_munka/user/dht.c
@@ -5,4 +5,67 @@
  *      Author: nolex
  */
 
+#include "dht.h"
 
+static uint8_t DhtReadSts;
+static os_timer_t DhtTimer;
+static uint8_t DhtResultIdx;
+static uint8_t DhtResult[42];
+static uint32_t DhtTimerOffset;
+
+void dhtGpioCbk()
+{
+
+}
+
+ICACHE_FLASH_ATTR void dhtGpioCbkSetup()
+{
+	ETS_GPIO_INTR_DISABLE();
+	GPIO_DIS_OUTPUT(DHT_PIN);
+	ETS_GPIO_INTR_ATTACH(&dhtGpioCbk, NULL);
+	gpio_register_set(GPIO_PIN_ADDR(DHT_PIN), GPIO_PIN_INT_TYPE_SET(GPIO_PIN_INTR_DISABLE)
+				| GPIO_PIN_PAD_DRIVER_SET(GPIO_PAD_DRIVER_DISABLE)
+				| GPIO_PIN_SOURCE_SET(GPIO_AS_PIN_SOURCE));
+
+	GPIO_REG_WRITE(GPIO_STATUS_W1TC_ADDRESS, BIT(DHT_PIN));
+	gpio_pin_intr_state_set(GPIO_ID_PIN(DHT_PIN), GPIO_PIN_INTR_NEGEDGE);
+	ETS_GPIO_INTR_ENABLE();
+	DhtResultIdx = 0;
+	os_memset(&DhtResult, 0, sizeof(DhtResult));
+	DhtTimerOffset = WDEW_NOW();
+
+}
+
+void dhtTimerCbk()
+{
+	DHT_PIN_DIS();
+	dhtGpioCbkSetup();
+}
+
+ICACHE_FLASH_ATTR void dhtProcess()
+{
+
+}
+
+ICACHE_FLASH_ATTR void dhtInit()
+{
+	PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTMS_U, FUNC_GPIO14);
+	DHT_PIN_ON();
+	DhtReadSts++;
+}
+
+ICACHE_FLASH_ATTR void dhtStart()
+{
+	dhtInit();
+	os_delay_us(500);
+	DHT_PIN_OFF();
+	os_timer_disarm(&DhtTimer);
+	os_timer_setfn(&DhtTimer, (os_timer_t*)dhtTimerCbk, (void*)0);
+	os_timer_arm(&DhtTimer, 20, 0);
+
+}
+
+ICACHE_FLASH_ATTR void dhtTest()
+{
+
+}
